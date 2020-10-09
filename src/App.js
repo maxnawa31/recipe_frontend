569882c4a23a6665c1ed398bff6ec9a3e74d50ab
maxnawa31 <mnawa31@gmail.com>
2020-10-09 07:29:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
+import {createEditor} from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+  const handleChange = (newValue) => {
+    console.log(newValue)
+    setValue(newValue)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Slate editor={editor} value={value} onChange={(newValue) => handleChange(newValue)}>
+      <Editable />
+    </Slate>
+  )
 }
 
 export default App;
